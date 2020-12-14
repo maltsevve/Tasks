83a740b5963c9e8f74bebc9ba85db81a4a5f04f7
Victor Maltsev <maltsevve@gmail.com>
2020-12-14 15:10:35 +0300

    1.0

---
+++ src/main/java/com/maltsevve/Task01.java
@@ -0,0 +1,34 @@
+package main.java.com.maltsevve;
+
+public class Task01 {
+    static int[] findItems(int[] arr, int sum) {
+        for (int j : arr) {
+            for (int k : arr) {
+                if (j + k == sum) return new int[]{j, k};
+            }
+        }
+
+        return new int[]{0, 0};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 14, 64, 0, 43, 2, -8};
+
+        int[] temp = findItems(arr, 15);
+        for (int i : temp) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        temp = findItems(arr, -6);
+        for (int i : temp) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        temp = findItems(arr, 64);
+        for (int i : temp) {
+            System.out.print(i + " ");
+        }
+    }
+}
